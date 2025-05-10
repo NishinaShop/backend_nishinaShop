ae600a4e3bf71bec0dea2bf8e3f4e9a19994d073
Emanuel Morales <eemoralesb@outlook.com>
2025-05-10 13:02:24 -0600

    ma

--- models/direcciones.js
+++ models/direcciones.js
@@ -1,15 +1,15 @@
 const mongoose = require ('mongoose');
 var schema = mongoose.Schema;
 var direcciones_schema = schema({
-    nombre: {type: String, requiered: true},
-    calle: {type: String, requiered: true},
-    colonia: {type: String, requiered: true},
-    ciudad: {type: String, requiered: true},
-    codigo_postal: {type: String, requiered: true},
-    estado: {type: String, requiered: true},
-    email: {type: String, requiered: true},
-    n_contacto: {type: String, requiered: true},
-    cliente: {type: schema.ObjectId, ref: 'cliente', requiered: true},
+    nombre: {type: String, required: true},
+    calle: {type: String, required: true},
+    colonia: {type: String, required: true},
+    ciudad: {type: String, required: true},
+    codigo_postal: {type: String, required: true},
+    estado: {type: String, required: true},
+    email: {type: String, required: true},
+    n_contacto: {type: String, required: true},
+    cliente: {type: schema.ObjectId, ref: 'cliente', required: true},
     createdAt : {type: Date, default: Date.now}
 }); 
 module.exports = mongoose.model('direcciones',direcciones_schema)
