9c5baacf70a963dba90573e80bd528a7345a636e
Emanuel Morales <eemoralesb@outlook.com>
2025-05-11 14:29:00 -0600

    filtros

--- controllers/ventasControllers.js
+++ controllers/ventasControllers.js
@@ -4,11 +4,29 @@ var detalles_ventas = require ('../models/detalles_venta')
 
 const listar_ventas_admin = async function(req,res){
     if(req.user){
-        let venta = await ventas.find().populate('cliente').populate('direccion');
-        res.status(200).send(venta)
-    }else{
-        res.status(500).send({data: undefined, message:"ErroToken"});
-    }
+        try {
+                    let filtro = req.params['filtro'] || req.query['filtro'] || null;
+                    let query = {};
+            
+                    // Si hay filtro, agregamos las condiciones de búsqueda
+                    if(filtro) {
+                        query.$or = [
+                            {estado: new RegExp(filtro, 'i')},
+                            {serie: new RegExp(filtro, 'i')},
+                            {transaccion: new RegExp(filtro, 'i')},
+                        ];
+                    }
+            
+                    let venta = await ventas.find(query).sort({createdAt:-1});
+                    res.status(200).send(detalles_ventas);
+                    
+                } catch(error) {
+                    console.error(error);
+                    res.status(500).send({message: "Error al buscar la orden"});
+                }
+            }else{
+                res.status(500).send({data: undefined, message:"ErroToken"});
+            }
 }
 module.exports = {
 listar_ventas_admin,
