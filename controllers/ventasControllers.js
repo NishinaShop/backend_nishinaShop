8bc13906c6a709d568bedbf36d15a8904d73df1f
Emanuel Morales <eemoralesb@outlook.com>
2025-05-14 13:03:52 -0600

    listar ordenes

--- controllers/ventasControllers.js
+++ controllers/ventasControllers.js
@@ -2,32 +2,15 @@
 var ventas= require ('../models/venta')
 var detalles_ventas = require ('../models/detalles_venta')
 
-const listar_ventas_admin = async function(req,res){
-    if(req.user){
-        try {
-                    let filtro = req.params['filtro'] || req.query['filtro'] || null;
-                    let query = {};
-            
-                    // Si hay filtro, agregamos las condiciones de búsqueda
-                    if(filtro) {
-                        query.$or = [
-                            {estado: new RegExp(filtro, 'i')},
-                            {serie: new RegExp(filtro, 'i')},
-                            {transaccion: new RegExp(filtro, 'i')},
-                        ];
-                    }
-            
-                    let venta = await ventas.find(query).sort({createdAt:-1});
-                    res.status(200).send(venta);
-                    
-                } catch(error) {
-                    console.error(error);
-                    res.status(500).send({message: "Error al buscar la orden"});
-                }
-            }else{
-                res.status(500).send({data: undefined, message:"ErroToken"});
-            }
-}
+const listar_ordenes_admin = async function(req,res){
+     if(req.user){
+         let ordenes = await ventas.find({cliente:req.user.sub})
+         res.status(200).send({ordenes})
+     }else{
+         res.status(500).send({data: undefined, message: 'Error al validar el token'})
+     }
+ 
+ }
 module.exports = {
-listar_ventas_admin,
+listar_ordenes_admin,
 }
--- routes/ventas.js
+++ routes/ventas.js
@@ -3,6 +3,6 @@ var ventaController = require ('../controllers/ventasControllers')
 var authenticate = require ('../middlewares/authenticate')
 var api = express.Router();
 
-api.get('/listar_ventas_admin', authenticate.decodeToken, ventaController.listar_ventas_admin);
+api.get('/listar_ordenes_admin', authenticate.decodeToken, ventaController.listar_ordenes_admin);
 
 module.exports = api;
