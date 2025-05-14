0d74d104485b525583e268040596498c5fa1abc9
Emanuel Morales <eemoralesb@outlook.com>
2025-05-14 14:59:12 -0600

    Generada

--- controllers/ventasControllers.js
+++ controllers/ventasControllers.js
@@ -4,7 +4,7 @@ var detalles_ventas = require ('../models/detalles_venta')
 
 const listar_ordenes_admin = async function(req,res){
      if(req.user){
-         let ordenes = await ventas.find({cliente:req.user.sub})
+         let ordenes = await ventas.find({cliente:req.user.sub}).sort({createdAt:-1})
          res.status(200).send({ordenes})
      }else{
          res.status(500).send({data: undefined, message: 'Error al validar el token'})
