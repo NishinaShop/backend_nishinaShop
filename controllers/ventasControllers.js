396dfa2902ad944142ee43cf1bb755135178e719
Emanuel Morales <eemoralesb@outlook.com>
2025-05-15 08:56:40 -0600

    listar compras

--- controllers/ventasControllers.js
+++ controllers/ventasControllers.js
@@ -11,6 +11,33 @@ const listar_ordenes_admin = async function(req,res){
      }
  
  }
+ const listar_ventas_admin = async function(req,res){
+    if(req.user){
+        try {
+                    let filtro = req.params['filtro'] || req.query['filtro'] || null;
+                    let query = {};
+            
+                    // Si hay filtro, agregamos las condiciones de búsqueda
+                    if(filtro) {
+                        query.$or = [
+                            {estado: new RegExp(filtro, 'i')},
+                            {serie: new RegExp(filtro, 'i')},
+                            {transaccion: new RegExp(filtro, 'i')},
+                        ];
+                    }
+            
+                    let venta = await ventas.find(query).sort({createdAt:-1});
+                    res.status(200).send(venta);
+                    
+                } catch(error) {
+                    console.error(error);
+                    res.status(500).send({message: "Error al buscar la orden"});
+                }
+            }else{
+                res.status(500).send({data: undefined, message:"ErroToken"});
+            }
+}
 module.exports = {
 listar_ordenes_admin,
+listar_ventas_admin
 }
--- routes/ventas.js
+++ routes/ventas.js
@@ -4,5 +4,5 @@ var authenticate = require ('../middlewares/authenticate')
 var api = express.Router();
 
 api.get('/listar_ordenes_admin', authenticate.decodeToken, ventaController.listar_ordenes_admin);
-
+api.get ('/listar_ventas_admin', authenticate.decodeToken,ventaController.listar_ventas_admin);
 module.exports = api;
