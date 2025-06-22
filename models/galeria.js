af27669f8ee3ef9a9d0fa09468c6ce67353ae1a1
Emanuel Morales <eemoralesb@outlook.com>
2025-06-22 16:41:45 -0600

    Despliegue imagenes por color

--- models/galeria.js
+++ models/galeria.js
@@ -3,7 +3,7 @@ var schema = mongoose.Schema;
 var galeria_schema = schema({
     imagen: {type: String, required: true},
     producto: {type: schema.ObjectId, ref: 'producto', required: true},
-    color: {type: schema.ObjectId, ref: 'color,', required: true},
+    color: {type: schema.ObjectId, ref: 'color', required: true},
     createdAt : {type: Date, default: Date.now}
 }); 
 module.exports = mongoose.model('galeria',galeria_schema)
